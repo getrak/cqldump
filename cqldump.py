c61d4df5225f733b7229e901dda95d7d1c99e71c
Marcos Paulo Vaz <marcos.vaz@getrak.com.br>
2020-10-07 19:12:35 -0300

    Otimização de Consultas e estruturação do arquivo em classe e métodos

--- cqldump.py
+++ cqldump.py
@@ -5,50 +5,79 @@ from cassandra.cluster import Cluster
 from cassandra.query import dict_factory
 from cassandra.metadata import KeyspaceMetadata, TableMetadata
 from datetime import datetime
+from cassandra.query import SimpleStatement
 
-parser = ArgumentParser()
-parser.add_argument('--h',type=str) #HOST
-parser.add_argument('--w',type=str) #WHERE
-parser.add_argument('--k',type=str) #KEYSPACE
-parser.add_argument('--t',type=str) #TABLE
+class CQLDUMP():
 
-args = parser.parse_args()
+    cluster = ''
+    session = ''
 
-cluster = Cluster([args.h])
-session = cluster.connect(args.k)
-session.row_factory = dict_factory
+    def __init__(self):
+        inicio = datetime.now()
 
-columns =  cluster.metadata.keyspaces[args.k].tables[args.t].columns
+        parser = ArgumentParser()
+        parser.add_argument('--h',type=str) #HOST
+        parser.add_argument('--w',type=str) #WHERE
+        parser.add_argument('--k',type=str) #KEYSPACE
+        parser.add_argument('--t',type=str) #TABLE  
 
-# QUERY
-query = "select * from " + args.t
-if args.w:
-    query += " where "+args.w
-query += " limit 10;"
-rows = session.execute(query)
+        args = parser.parse_args() # GET PARAMS
+        self.connect(args.h, args.k) # CONNECT WITH CASSANDRA
+        query = self.read(args.t, args.w) # BUILD QUERY
+        self.write(query, args.k, args.t) # WRITE IN .CQL FILE
 
-str_columns = ""
-for col in columns:
-        str_columns += col+", "
+        fim = datetime.now()
+        print(f'Início: {inicio}')
+        print(f'Fim: {fim}')
+        print(f'Tempo total: {fim - inicio}')
+        print("Dump successfully exported!")
 
-# WRITE IN FILE .CQL
-data_atual = datetime.now().strftime('%d%m%Y%H%M%S')
-with open(f'cqldump_{data_atual}.cql', 'w') as f:
+    def connect(self, host, keyspace):
+        global cluster 
+        global session
+        cluster = Cluster([host])
+        session = cluster.connect(keyspace)
+        session.row_factory = dict_factory
 
-    create_keyspace_sql = cluster.metadata.keyspaces[args.k].as_cql_query().replace("CREATE KEYSPACE","CREATE KEYSPACE IF NOT EXISTS")
-    create_table_sql = cluster.metadata.keyspaces[args.k].tables[args.t].export_as_string().replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS").replace("}'","}").replace("caching = '{","caching = {").replace('"',"'")
-
-    f.write(create_keyspace_sql)
-    f.write(f"; \n\nUSE {args.k};\n\n")
-    f.write(create_table_sql)
     
-    for row in rows:
-        vals = ""
-        f.write(f"\n\nINSERT INTO {args.t} ({str_columns[:(len(str_columns) - 2)]}) VALUES (")
+    def read(self,table, where):
+        query = f"SELECT * FROM {table}"
+        if where:
+            query += f" WHERE {where} ALLOW FILTERING"
+        return query
+
+    def write(self, query, keyspace, table):
+        global cluster
+        global session
+        keyspace_metadata = cluster.metadata.keyspaces[keyspace]
+
+        columns =  keyspace_metadata.tables[table].columns
+        str_columns = ""
         for col in columns:
-            vals += (f"{row[col]}, ") if isinstance(row[col], int) else (f"'{row[col]}', ")
-        f.write(f"{vals[:(len(vals) - 2)]});")
+            str_columns += col+", "
+        str_columns = str_columns[:(len(str_columns) - 2)]
+            
+        name_file = f'cqldump_{datetime.now().strftime("%d%m%Y%H%M%S")}.cql'
+        
+        with open(name_file, 'w') as f:
+            create_keyspace_sql = keyspace_metadata.as_cql_query().replace("CREATE KEYSPACE","CREATE KEYSPACE IF NOT EXISTS")
+            create_table_sql = keyspace_metadata.tables[table].export_as_string().replace("CREATE TABLE", "CREATE TABLE IF NOT EXISTS").replace("}'","}").replace("caching = '{","caching = {").replace('"',"'")
 
-f.close
+            f.write(create_keyspace_sql)
+            f.write(f"; \n\nUSE {keyspace};\n\n")
+            f.write(create_table_sql)
 
-print("Dump successfully exported!")
+            statement = SimpleStatement(query, fetch_size=100000)
+            for row in session.execute(statement):
+
+                values = ""
+                for col in columns:
+                    values += (f"{row[col]}, ") if isinstance(row[col], int) else (f"'{row[col]}', ")
+                values = values[:(len(values) - 2)]
+                
+
+                f.write(f"\nINSERT INTO {table} ({str_columns}) VALUES ({values});")
+
+        f.close
+
+cqldump = CQLDUMP()
